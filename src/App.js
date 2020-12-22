cf4e0b0dd1ea97c0a8de764fce0b66595becb20a
Emma Mendez <emmamendez07@gmail.com>
2020-12-22 15:00:50 +0000

    my app with logo in place and some understanding of how to build forward afteer mentoring session... working on buttons to connect to backend next

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='acslogo.png' className="App-logo" alt="logo" />
       </header>
+      <h1>VOCAL TRAINER</h1>
+      <button
+        style={{ color: 'red', backgroundColor: 'black' }}
+      onClick={() => window.alert ('You clicked me')}>Press me</button>
+      <Avocado />
     </div>
   );
 }
+
+function Avocado() {
+  return (
+    <>
+    <button style={{ backgroundColor: 'orange', marginRight: 50 }}
+    onClick={() => window.alert ('Ummmmmmmmmm')}>Yummy!</button>
+    <button
+      onClick={() => window.alert("Goodbye!")}
+    >Sign Up</button>
+    </>
+  )
+}
+
+// function sayHello() {
+//   return "MAgic beans from the market"
+// }
+
+
+
 
 export default App;
