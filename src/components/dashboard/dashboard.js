8f58da3d314478c718b1f96c1f320bad04098cb4
harjeet <harjeet.kalsi@solirius.com>
2021-01-17 17:13:02 +0000

    Add submitting data from form to mongo

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -5,12 +5,15 @@ class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
-      show: false
+      show: false,
+      email: '',
+      password: ''
     };
     this.showModalLogin = this.showModalLogin.bind(this);
     this.hideModalLogin = this.hideModalLogin.bind(this);
     this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
     
   }
 
@@ -31,29 +34,76 @@ class Dashboard extends Component {
     this.setState({ show: false });
   };
 
-  
+  onSubmitLogin = (data) => {
+    console.log('Submit data: ', data);
+    console.log(this.state.email);
+    console.log(this.state.password);
+    this.handleLogin();
+  };
+
+  onSubmitRegister = (data) => {
+   //need to write this
+  };
+
+  onEmailChange = (event) => {
+      this.setState({
+          email: event.target.value
+      });
+  }
+
+    onPasswordChange = (event) => {
+      this.setState({
+          password: event.target.value
+      });
+  }
+
+  handleLogin =async () => {
+    // const response = await fetch('http://localhost:1000/signup', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept' : 'application/json',
+    //     'Content-Type' : 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     userName: this.state.email,
+    //     password: this.state.password
+    //   })
+    // })
+    // const json = await response.json()
+    // window.alert(json.message)
+  console.log("Good to GO Emma!")
+
+  }
+
+
 
     render() {
         return (
           <main>
-              <Modal show={this.state.showLogin} handleClose={this.hideModalLogin}>
-          <h1>Log In</h1>
-    <form  action="/login" method="POST">
-    <div>
-        <label class="label" for="email">Email</label>
-        <input class="text-box" type="email" id="email" name="email"
-        required/>
-    </div>
-    <div>
-        <label class="label" for="password">Password</label>
-        <input class="text-box" class="text-box" type="password" id="password" name="password"
-        required/>
-    </div>
-    </form>
+              <Modal show={this.state.showLogin}>
+                <h1>Log In</h1>
+                <form action="\studio" onSubmit={this.onSubmitLogin}>
+                    <div>
+                        <label class="label" for="email">Email</label>
+                        <input class="text-box" type="email" id="email" name="email" value={this.state.email} onChange={this.onEmailChange}
+                        required/>
+                    </div>
+                    <div>
+                        <label class="label" for="password">Password</label>
+                        <input class="text-box" class="text-box" type="password" id="password" name="password" value={this.state.password} onChange={this.onPasswordChange}
+                        required/>
+                    </div>
+                    <button className="buttonf" type="button" onClick={this.hideModalLogin}>
+                        Close
+                    </button>
+                    <button className="buttonf" type="submit">
+                        Login
+                    </button>
+                </form>
         </Modal>
-        <Modal show={this.state.show} handleClose={this.hideModal}>
+        <Modal show={this.state.show}>
           <h1>Register</h1>
-    <form class="text" action="/register" method="POST">
+    <form onSubmit={this.onSubmitRegister}>
     <div>
         <label class="label" for="name">Choose UserName</label>
         <input class="text-box" type="text" id="name" name="name" value={this.state.cname}
@@ -74,6 +124,12 @@ class Dashboard extends Component {
         <input class="text-box" type="password" id="password" name="password" value={this.state.password}
         required/>
     </div>
+       <button className="buttonf" type="button" onClick={this.hideModalLogin}>
+                        Close
+       </button>
+        <button className="buttonf" type="submit">
+                        Register
+        </button>
     </form>
         </Modal>
             <button class="text-box" type="button" class="button" onClick={this.showModalLogin}>
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,87 +1,24 @@
 import './modal.css';
 import React from "react";
-import {Button} from "react-bootstrap";
 
 const Modal = ({ handleClose, show, children }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
   const showHideRegistration = show ? "modal display-block" : "modal display-none";
 
 
-  const submitButton =async () => {
-    const response = await fetch('http://localhost:1000/example-one', {
-      method: 'POST',
-      headers: {
-        'Accept' : 'application/json',
-        'Content-Type' : 'application/json'
-      },
-      body: JSON.stringify({
-        user: 'details'
-      })
-    })
-    
-    const json = await response.json()
-    window.alert(json.message)
-  }
-
-  const handleRegister =async () => {
-    // const response = await fetch('http://localhost:1000/signup', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Accept' : 'application/json',
-    //     'Content-Type' : 'application/json'
-    //   },
-    //   body: JSON.stringify({
-    //     userName: 'mr blobby',
-    //     password: 'pink123'
-    //   })
-    // })
-    // const json = await response.json()
-    // window.alert(json.message)
-  console.log("Good to GO Emma!")
-    console.log({children})
-    // console.log("cname: " + this.state.cname);
-    // console.log("Email: " + this.state.email);
-  }
-
 
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
         {children}
-        <button class="buttonf" type="button" onClick={handleClose}>
-          Close
-        </button>
-          <button class="buttonf" type="button" onClick={ ""}>
-            <button onClick={handleRegister}>Register</button>
-          {/* <Button href="/home">Log In</Button> */}
-        </button>
       </section>
     </div>
   );
 
 
-  // return (
-  //   <div className={showHideRegistration}>
-  //     <section className="modal-main">
-  //       {children}
-  //       <button class="buttonf" type="button">
-  //         Close
-  //       </button>
-  //       <button class="buttonf" type="button" onClick={handleClose}>
-  //         Submit
-  //       </button>
-  //     </section>
-  //   </div>
-  // )
 };
 
 
 
 
 export default Modal;
-
-
-// 
-{/* <button class="buttonf" type="button" onClick={handleClose}>
-Close
-</button> */}
