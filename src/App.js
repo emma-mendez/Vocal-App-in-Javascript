18d0c8f102bb7f538705d922a0ded9184218690a
Emma Mendez <emmamendez07@gmail.com>
2021-02-03 22:00:55 +0000

    code fixed and working as should be ... sound code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import CassetteTape from "./components/CassetteTape";
 import { createGlobalStyle } from "styled-components";
 import styledNormalize from "styled-normalize";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './pages/home.js'
+import Home from './pages/Home.js'
 import Page2 from './pages/page2';
 import Page3 from './pages/page3';
 import Journal from './pages/journal';
