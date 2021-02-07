c32dbb6e634b3061c639d84ec6d56725925180b9
Emma Mendez <emmamendez07@gmail.com>
2021-02-07 13:02:42 +0000

    settled on the blue colour for studio

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ class App extends Component {
             <Route exact path="/studio" render={() => (
                 <>
            <GlobalStyles />
-        <CassetteTape vizColor="blue" />
+        <CassetteTape vizColor="#0247FE" />
             </>
              )} />
              <Route exact path="/game" render={() => (
